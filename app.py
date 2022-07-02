75351ee4995b34cba7ebe51fce201f3f0f751aaf
Eric Thomas <eric@ericthomas.ca>
2022-07-02 17:12:17 -0230

    connect to api, printing jsons

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+import requests
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+open_weather_map_api = '38eaed335d3085d2ca20fa104799a742'
+city_url = 'http://api.openweathermap.org/data/2.5/weather?appid=' + \
+            open_weather_map_api + '&id='
 
 
 @app.route('/')
 def index():  # put application's code here
-    return '<h1>Welcome to Eric\'s Weather App!</h1>'
+    return render_template("index.html")
+
+
+@app.route('/<city_id>')
+def weather(city_id):
+    response = requests.get(city_url + city_id)
+    weather_data = json.loads(response.text)
+
+    return render_template("weather.html", weather=weather_data)
 
 
 if __name__ == '__main__':
